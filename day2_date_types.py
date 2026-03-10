fceb48c5f0ca22bd518d209d94209fab4c4f5d4c
ldhyb-dh <ldhyb.dh@gmail.com>
2026-03-10 21:44:21 +0900

    Add files via upload
    Add Day 2 - 숫자형 문자열 불린 자료형

---
+++ day2_date_types.py
@@ -0,0 +1,30 @@
+#Day2 -숫자형, 문자열 ,불린형
+
+#1. 숫자형(int, float)
+print(5)
+print(3.14)
+print(5 + 3.14)
+
+#2. 문자열(str)
+print("부산")
+print('해운대')
+print("오늘 날씨는 " + " 맑음 ")
+
+#3. 불린형(bool)
+print(5 > 10) #False
+print(5 < 10) #true
+print( not True)
+
+#4.type() 함수 
+print(type(5)) # int
+print(type(3.14)) # flaot
+print(type("부산")) # str
+print(type(True))  #bool
+
+age = 25 
+print(type(age))
+
+city ="부산"
+print(city + " 최고!")
+is_sunny = True
+print(not is_sunny)
